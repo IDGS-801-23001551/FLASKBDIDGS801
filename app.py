de49bbae883a7ae611e16c70d0c5dbf519c0fd24
Emiliano Mendoza <253613665+IDGS-801-23001551@users.noreply.github.com>
2026-02-25 18:00:31 -0600

    abc completo 25/02/2026

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask import flash
 from flask_wtf.csrf import CSRFProtect
 from flask import g
+from flask_migrate import  Migrate
 
 from config import DevelopmentConfig
 import forms
@@ -10,6 +11,8 @@ from models import db, Alumnos
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 csrf=CSRFProtect()
+db.init_app(app)
+migrate=Migrate(app, db)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -68,9 +71,31 @@ def modificar():
 		return redirect(url_for('index'))
 	return render_template("Modificar.html",form=create_form)
 
+@app.route('/eliminar', methods=['GET','POST'])
+def eliminar():
+	create_form=forms.UserForm2(request.form)
+	if request.method=='GET':
+		id = request.args.get('id')
+		alum1 = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+		if alum1:
+			create_form.id.data = alum1.id
+			create_form.nombre.data = alum1.nombre
+			create_form.apaterno.data = alum1.apaterno
+			create_form.correo.data = alum1.email
+			return render_template("eliminar.html", form=create_form)
+	
+	if request.method=='POST':
+		id=create_form.id.data
+		alum= db.session.query(Alumnos).filter(Alumnos.id == id).first()
+		if alum:
+			db.session.delete(alum)
+			db.session.commit()
+			return redirect(url_for('index'))
+	return render_template("eliminar.html", form=create_form)
+
 if __name__ == '__main__':
 	csrf.init_app(app)
-	db.init_app(app)
+	
 	with app.app_context():
 		db.create_all()
 	app.run(debug=True)
